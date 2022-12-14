65b5bca47a1b65a8a7dde3942b3102a36d4c3961
Olamilekan <olamilekanomosanya@gmail.com>
2022-12-14 18:01:51 +0100

    feat: nattivebalance endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,36 @@
+const Moralis = require("moralis").default;
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 8080;
+require("dotenv").config();
 
-app.use(cors());
+app.use(cors()); 
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
-});
+  res.send("Hello World  wrrr!");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+})
+
+
+app.get("/nativeBalance", async (req, res) => {
+  await Moralis.start({apiKey: process.env.MORALIS_API_KEY });
+
+  try {
+    const {address, chain} =req.query;
+    const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+      address: address,
+      chain: chain,
+    });
+    const nativeBalance = response.data;
+     res.send(nativeBalance);
+  } catch (e) {
+    res.send(e);
+    
+  }
+
+ 
 });
